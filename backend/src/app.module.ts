db05a01af88f19b58478dc6fb5df86d1537bb2c5
Brian Sanabria <brisaning@gmail.com>
2025-04-22 07:38:53 -0500

    estructura APIs base

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CompaniesModule } from './companies/companies.module';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { LocationsModule } from './locations/locations.module';
 
 @Module({
-  imports: [],
+  imports: [CompaniesModule, ProductsModule, UsersModule, LocationsModule],
   controllers: [],
   providers: [],
 })
