c43b2238629aba152c34d33dd9d7cdd43b662d92
Brian Sanabria <brisaning@gmail.com>
2025-04-22 08:03:20 -0500

    Entidades compañias y productos

--- backend/src/companies/entities/company.entity.ts
+++ backend/src/companies/entities/company.entity.ts
@@ -1,1 +1,37 @@
-export class Company {}
+import { Product } from "src/products/entities/product.entity";
+import { User } from "src/users/entities/user.entity";
+import { Column, Entity, JoinTable, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Company {
+    @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  sector: string;
+
+  @ManyToOne(() => City, city => city.companies)
+  city: City;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  address: string;
+
+  @Column('decimal', { precision: 15, scale: 2 })
+  assets: number;
+
+  @Column('decimal', { precision: 15, scale: 2 })
+  liabilities: number;
+
+  @ManyToMany(() => Product)
+  @JoinTable()
+  products: Product[];
+
+  @OneToMany(() => User, user => user.company)
+  users: User[];
+}
--- backend/src/products/entities/product.entity.ts
+++ backend/src/products/entities/product.entity.ts
@@ -1,1 +1,20 @@
-export class Product {}
+import { Company } from "src/companies/entities/company.entity";
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn()
+    id: number;
+    
+    @Column()
+    name: string;
+    
+    @Column()
+    category: string;
+    
+    @Column('decimal', { precision: 10, scale: 2 })
+    price: number;
+    
+    @ManyToMany(() => Company, company => company.products)
+    companies: Company[];
+}
