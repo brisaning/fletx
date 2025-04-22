c3e54c7193ba20abd55663cdd2369e59eba63430
Brian Sanabria <brisaning@gmail.com>
2025-04-22 07:49:40 -0500

    configuración base de datos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,31 @@ import { CompaniesModule } from './companies/companies.module';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { LocationsModule } from './locations/locations.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CompaniesModule, ProductsModule, UsersModule, LocationsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      //entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CompaniesModule,
+    ProductsModule,
+    UsersModule,
+    LocationsModule
+  ],
   controllers: [],
   providers: [],
 })
