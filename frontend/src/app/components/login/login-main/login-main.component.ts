7b38c8f7a497115fba158a6c7dfe276abe970328
Brian Sanabria <brisaning@gmail.com>
2025-04-29 09:47:47 -0500

    se completa flujo login

--- frontend/src/app/components/login/login-main/login-main.component.ts
+++ frontend/src/app/components/login/login-main/login-main.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { LoginService } from '../../../../servicies/login.service';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-main',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login-main.component.html',
   styleUrl: './login-main.component.scss',
   providers: [LoginService],
@@ -12,13 +14,17 @@ import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 export class LoginMainComponent {
 
   loginForm: FormGroup;
+  isLoading: boolean = false;
+  errorMessage: string | null = null;
+
   constructor(
     private loginService: LoginService,
-    private formBuilder: FormBuilder,
+    private fb: FormBuilder,
+    private router: Router
   ) {
-    this.loginForm = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl(''),
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
@@ -26,18 +32,37 @@ export class LoginMainComponent {
   }
 
   onLogin() {
+    if (this.loginForm.invalid) {
+      this.errorMessage = 'Please fill in all required fields correctly.';
+      console.log('Form is invalid');
+      return;
+    }
+    this.isLoading = true;
+    this.errorMessage = null;
+    this.loginForm.markAllAsTouched();
+    this.loginForm.markAsPristine();
+
+    const {email, password} = this.loginForm.value;
+
     let params = {
-      email: this.loginForm.value.email.getRawValue(),
-      password: this.loginForm.value.password.getRawValue(),
+      email,
+      password
     }
 
-    console.log({'Login button clicked': params, "loginForm": this.loginForm});
+    console.log({'Login button clicked': params, "loginForm": this.loginForm.value});
     this.loginService.login(params).subscribe(
       response => {
         console.log('Login successful', response);
+        localStorage.setItem('auth_token', response.accessToken);
+        localStorage.setItem('user', JSON.stringify(response.user));
+        this.router.navigate(['/']);
         // Handle successful login, e.g., navigate to another page
       }
     );
+  }
+
+  getRegsiter() {
+    this.router.navigate(['/register']);
   }
   
 
